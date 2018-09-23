7c7b95f044c32b16dfee591277ee8aad9d875d97
Gracius Fafane <fannygracius@gmail.com>
2018-09-23 19:34:33 -0400

    Termine
    Signed-off-by: Gracius Fafane <fannygracius@gmail.com>

---
+++ app/src/main/java/com/example/fafoo/simpletodo/EditItemActivity.java
@@ -0,0 +1,37 @@
+package com.example.fafoo.simpletodo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class EditItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item);
+
+        // take the select text in mainActivity with his position and put it in EditActivity
+        String textBody = getIntent().getStringExtra("text");
+        EditText etText = findViewById(R.id.etText);
+        etText.setText(textBody);
+        etText.setSelection(etText.getText().length());
+    }
+
+    public void onSubmit (View v){
+        EditText etText = findViewById(R.id.etText);
+        Intent data = new Intent();
+        data.putExtra("text", etText.getText().toString());
+        int textPosition = getIntent().getIntExtra("position", 0);
+        data.putExtra("position", textPosition);
+        setResult(RESULT_OK, data);
+        finish();
+
+    }
+
+    public void onCancel (View v){
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/fafoo/simpletodo/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.fafoo.simpletodo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView LvItems;
+    EditText etEditText;
+    private final int REQUEST_CODE=20;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        LvItems = (ListView) findViewById(R.id.LvItems);
+        readItems();
+        itemsAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, items);
+        LvItems.setAdapter(itemsAdapter);
+        SetupListViewListener();
+    }
+
+    private void SetupListViewListener() {
+        LvItems.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter,
+                                                   View item, int pos, long id) {
+                        items.remove(pos);
+                        itemsAdapter.notifyDataSetChanged();
+                        writeItems();
+                        return true;
+                    }
+
+                });
+
+        LvItems.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Intent i = new Intent(getApplicationContext(), EditItemActivity.class);
+                         //put "extras" into the bundle for access in the second activity
+                                i.putExtra("text", items.get(position));
+                                i.putExtra("position", position);
+                        // brings up the second activity
+                        startActivityForResult(i, REQUEST_CODE);
+                    }
+
+                    @Nullable
+                        public final AdapterView.OnItemClickListener getOnItemClickListener() {
+                        return null;
+                    }
+
+
+                });
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<String>();
+        }
+
+
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+        writeItems();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK && requestCode == REQUEST_CODE){
+          String text = data.getExtras().getString("text");
+          int position = data.getExtras().getInt("position", 0);
+          items.set(position,text);
+          itemsAdapter.notifyDataSetChanged();
+          writeItems();
+        }
+
+    }
+}
+
+
+
+
+
